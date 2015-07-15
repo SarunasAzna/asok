1f4306ed8897d26569de932a915f1692305b6fbb
sarunasazna <sernas@fidi.lt>
2015-07-15 18:28:56 +0300

    edit and delete functions added, there is some bugs with frontend

--- flask/app/api/routes.py
+++ flask/app/api/routes.py
@@ -14,14 +14,12 @@ def index():
 @api.route('/locations', methods=['GET', 'POST'])
 def locations():
     if request.method == 'GET':
-        locs = readDatabase()
-
+        locs = read_database()
         return jsonify(locations=locs)
-
     elif request.method == 'POST':
         data = request.data.decode('utf-8')
         data = json.loads(data)
-        insertDatabase(data)
+        insert_database(data, data['delete'])
         # FIXME(FIXED): should return something meaningful to frontend to indicate the
         # success/failure
         # 
@@ -29,8 +27,7 @@ def locations():
 
 
 ## Function for sqlite database reading.
-def readDatabase():
-   
+def read_database():
     # FIXME(FIXED): we are reading from the database- there's nothing to save \Justas
     # Save (commit) the changes
     # 
@@ -46,35 +43,39 @@ def readDatabase():
         c = conn.cursor()
         locs = []
         for row in c.execute('SELECT * FROM locations ORDER BY lat'):
-            loc = OrderedDict({'title': row[0],
-                               'description': row[1],
-                               'lat': row[2],
-                               'lng': row[3]})
+            loc = OrderedDict({'id': row[0],
+                                'title': row[1],
+                                'description': row[2],
+                                'lat': row[3],
+                                'lng': row[4]})
             locs.append(loc)      
-
-
     return locs
-
-
 
 ## Function for sqlite database updatings.
 # @return Array : array with new locations information dictionarys
-def insertDatabase(location):
-  
+def insert_database(location, delete=False):
     with sqlite3.connect('./app/api/locations/locations.db') as conn:
-        c = conn.cursor()
-        locs = []
-        newLocs = (location['title'],
-                location['description'],
-                location['lat'],
-                location['lng'])
-        # FIXME(FIXED): executemany? We're only inserting a single location
-        c.execute('INSERT INTO locations VALUES (?,?,?,?)', newLocs)
-        # FIXME(FIXED): why do we want to retrieve locations after inserting?
-        # Save (commit) the changes
-        conn.commit()     
+        c = conn.cursor() 
+        if delete == False:
+            locs = (location['id'],
+                    location['title'],
+                    location['description'],
+                    location['lat'],
+                    location['lng'],
+                    
+                    )
+            try:
+                 # FIXME(FIXED): executemany? We're only inserting a single location
+                c.execute('INSERT INTO locations VALUES (?,?,?,?,?)', locs)
+                # FIXME(FIXED): why do we want to retrieve locations after inserting?    
+            except:
+                c.execute('''UPDATE locations SET title = ?, description = ?, lat = ?, lng = ? WHERE id = ? ''',
+                    (locs[1], locs[2], locs[3], locs[4], locs[0]))
+        else:
+            c.execute('''DELETE FROM locations WHERE id = ? ''', (location["id"],))  
+            # Save (commit) the changes
+            conn.commit()     
 
-    return locs
 
 
 
--- flask/manage.py
+++ flask/manage.py
@@ -16,7 +16,7 @@ manager = Manager(create_app)
 ## Function for sqlite database creation.
 # Old database should be deleted.
 @manager.command
-def createDatabase():
+def create_database():
     # FIXME: shouldn't we close the connection? Try doing it with "with"
     # statement
     # FIXED
@@ -26,22 +26,22 @@ def createDatabase():
                                         default=True, 
                                         yes_choices="y",
                                         no_choices="n")
-    dataBase = './app/api/locations/locations.db'
+    database = './app/api/locations/locations.db'
     if choise == True:
         try:
-            os.remove(dataBase)
+            os.remove(database)
         except OSError:
             pass
-        with sqlite3.connect(dataBase) as conn:
+        with sqlite3.connect(database) as conn:
             c = conn.cursor()
             # Create table
             c.execute('''CREATE TABLE locations
-                    (title text, description text,  lat real, lng real)''')
-            locations = [('Vilnius', 'Capital of Lithuania', 54.6833, 25.2833),
-                     ('Aarhus', 'Second largest city in Denmark', 56.1572, 10.2107),
-                     ('Hell', 'Norwegian hell', 63.4444, 10.9225),
-                     ('Fucking', 'Fucking, Austria', 48.0672, 12.8636)]
-            c.executemany('INSERT INTO locations VALUES (?,?,?,?)', locations) 
+                    (id INTEGER PRIMARY KEY, title TEXT, description TEXT,  lat TEXT, lng TEXT)''')
+            locations = [(1, 'Vilnius', 'Capital of Lithuania', 54.6833, 25.2833),
+                     (2, 'Aarhus', 'Second largest city in Denmark', 56.1572, 10.2107),
+                     (3, 'Hell', 'Norwegian hell', 63.4444, 10.9225),
+                     (4, 'Fucking', 'Fucking, Austria', 48.0672, 12.8636)]
+            c.executemany('INSERT INTO locations VALUES (?,?,?,?,?)', locations) 
             conn.commit()
 
 
